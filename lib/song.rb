0882243e17ab3fc78a8a43bb3865f3000846f726
Joseph cohen <jojocohen57@gmail.com>
2020-07-13 23:28:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,9 +41,13 @@ class Song
   def self.artist_count
      @@artists.each do |artist|
        if artist_count[artist] 
-         artist_count[artist] += 1
+         artist_count[artist] 
          artist_count[artist] += 1 
-       end 
+       else
+         artist_count[artist] = 1
+       end    
+     end 
+    return 
   end 
   
 end  
